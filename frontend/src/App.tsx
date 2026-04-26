b1556d12f2db39d6c96f4ff627e108314ae1b221
Newt20 <fatinkhan.97@gmail.com>
2026-04-26 16:12:49 +0600

    Remove auth duplicates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,17 @@ import Login from "./pages/Login";
 import Cart from "./components/Cart";
 import Register from "./pages/Register";
 import ProductList from "./components/ProductList";
-import { useAuth } from "./context/AuthContext";
 
 export default function App() {
-  const { userId } = useAuth(); 
-
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1">
         <Routes>
-          <Route path="/" element={<Home userId={userId} />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
 	  <Route path="/register" element={<Register />} />
-          <Route path="/cart" element={<Cart userId={userId} />} />
+          <Route path="/cart" element={<Cart />} />
 	  <Route path="/products" element={<ProductList />} />
         </Routes>
       </main>
