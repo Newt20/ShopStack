df36ad78e1f79472d1b706d5b6144ea83213bc62
Newt20 <47146755+Newt20@users.noreply.github.com>
2026-05-06 20:53:10 +0600

    Update login API endpoint to relative path

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -13,7 +13,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/api/users/login", { email, password });
+      const res = await axios.post("/api/users/login", { email, password });
       setUserId(res.data._id);
       alert(`Welcome ${res.data.name}!`);
       navigate("/");
